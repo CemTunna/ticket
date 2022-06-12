0b37263036224bfdf295e7f1e86784efb0630c06
CemTunna <cemroot@gmail.com>
2022-06-12 21:15:38 +0300

    get user from localstorage

--- frontend/src/state/features/auth/authSlice.ts
+++ frontend/src/state/features/auth/authSlice.ts
@@ -1,6 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+
+const user = JSON.parse(localStorage.getItem('user')!);
+
 const initialState = {
-  user: null,
+  user: user ? user : null,
   isError: false,
   isSuccess: false,
   isLoading: false,
